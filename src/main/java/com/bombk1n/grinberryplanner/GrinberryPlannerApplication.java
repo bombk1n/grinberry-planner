2b3b2e02112f287132e05a559716b3f8284e37c0
Roman Grinenko <grinenkor2110@gmail.com>
2025-05-05 11:43:34 +0300

    feat: init java spring boot project

---
+++ src/main/java/com/bombk1n/grinberryplanner/GrinberryPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.bombk1n.grinberryplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrinberryPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrinberryPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bombk1n/grinberryplanner/GrinberryPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bombk1n.grinberryplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrinberryPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
