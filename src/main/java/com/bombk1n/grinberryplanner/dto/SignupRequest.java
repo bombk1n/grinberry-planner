0403388425355d0e6e3a0b2a74fb219db33de4fb
Roman Grinenko <grinenkor2110@gmail.com>
2025-05-10 16:31:50 +0300

    feat: add DTO classes for authentication and user registration: JwtAuthResponse, SigninRequest, and SignupRequest

---
+++ src/main/java/com/bombk1n/grinberryplanner/dto/SignupRequest.java
@@ -0,0 +1,30 @@
+package com.bombk1n.grinberryplanner.dto;
+
+import com.bombk1n.grinberryplanner.entity.UserRole;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignupRequest {
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 5, message = "Password must be at least 5 characters long")
+    private String password;
+
+    private Set<UserRole> roles;
+
+    public @NotBlank(message = "Username is required") String getUsername() {
+        return username;
+    }
+
+
+}
